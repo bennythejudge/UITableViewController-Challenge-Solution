7544a68a0c09603e7fecc06908dcc2bc6ff0b82d
logube <m253ou@gmail.com>
2014-07-12 23:46:43 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/BTJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTJAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by neo on 12/07/2014.
+//  Copyright (c) 2014 BennyTheJudge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/BTJViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTJViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by neo on 12/07/2014.
+//  Copyright (c) 2014 BennyTheJudge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTJViewController : UIViewController
+
+@end
